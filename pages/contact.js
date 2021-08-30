de16c28f286cb2c53c8441676a280818992fd0fb
yasaman forouzesh <yasamanforouzesh93@gmail.com>
2021-08-29 18:00:19 -0700

    fix contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,3 @@
-import styles from "../styles/Contact.module.css"
 import Connection from "../components/Conection"
 import Header from "../components/Header"
 import COnnection from "../components/Conection"
@@ -46,33 +45,33 @@ export default function contact(){
         
     return(
         <>  
-            <div className={styles.container}>
-                <h1 className={styles.h1}>contact</h1>
-                < form className={styles.form} name="contact" method='POST' data-netlify='true'>
+            <div className="container-contact">
+                <h1 className="contact">contact</h1>
+                < form className="form" name="contact" method='POST' data-netlify='true'>
                 <input type='hidden' name='form-name' value='contact' />
                 
                 <p>
-                    <label className={styles.labal} htmlFor='name' >Name</label>
-                    <input  placeholder="Name" className={styles.input} onChange={(e)=>{setName(e.target.value)}} type='text' value={name} id='name' name='name'></input>
+                    <label className="labal" htmlFor='name' >Name</label>
+                    <input  placeholder="Name" className="input" onChange={(e)=>{setName(e.target.value)}} type='text' value={name} id='name' name='name'></input>
                 </p>
             
                 <p>
-                    <label className={styles.labal} htmlFor='email'>Email</label>
-                    <input className='contact-field' className={styles.input} placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}} type='text' value={email} id='email' name='email'></input>
+                    <label className="labal" htmlFor='email'>Email</label>
+                    <input className="input" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}} type='text' value={email} id='email' name='email'></input>
                 </p>
 
                 <p>
-                    <label  className={styles.labal} htmlFor='message'>Message</label>
-                    <textarea className={styles.content}  placeholder="Message" onChange={(e)=>{setBody(e.target.value)}} value={body} id='message' name='message'></textarea>
+                    <label  className="labal" htmlFor='message'>Message</label>
+                    <textarea className="content"  placeholder="Message" onChange={(e)=>{setBody(e.target.value)}} value={body} id='message' name='message'></textarea>
                 </p>
 
-                <p className={styles.labal}>
-                    < input className={styles.button} onClick={(e)=>{submitForm(e)}} type='submit' value='Send' /> { submitted ? <span className={styles.labal}> {submittedMsg} </span> : null }
+                <p className="labal">
+                    < input className="button" onClick={(e)=>{submitForm(e)}} type='submit' value='Send' /> { submitted ? <span className={styles.labal}> {submittedMsg} </span> : null }
                 </p>
             </form>
             </div>
-            <h1 className={styles.bye}>I'm so excited to conect with you and hear back from you. Thanks to see my portfolio.</h1>
-            <img className={styles.ex} src="/girl.jpg"/>
+            <h1 className="bye">I'm so excited to conect with you and hear back from you. Thanks to see my portfolio.</h1>
+            <img className="ex" src="/girl.jpg"/>
             <COnnection/>
             <Header active="contact"/>
         </>
